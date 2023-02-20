0bc286cca00d80ab22e029ec0f91b268c55a0cde
Abhishek Rawat <abhisheksabhi33@gmail.com>
2023-02-20 13:36:45 +0530

    changing baseurl 2

--- src/api/client.js
+++ src/api/client.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // https://rmovie.onrender.com/api
 
-const baseurl = "https://rmovie.onrender.com/api"
+// const baseurl = "https://rmovie.onrender.com/api"
 
-const client = axios.create({ baseURL: baseurl});
+const client = axios.create({ baseURL: "https://rmovie.onrender.com/api"});
 
 export default client;
