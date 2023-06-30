c19f4a9c97056feb3b4191c2c365626b705ad80d
Abhishek Rawat <abhisheksabhi33@gmail.com>
2023-06-30 18:13:35 +0530

    hashroute

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 
 import "./index.css";
@@ -12,9 +12,10 @@ const root = ReactDOM.createRoot(rootElement);
 
 
 root.render(
-  <BrowserRouter>
+  <HashRouter >
     <ContextProviders>
       <App />
     </ContextProviders>
-  </BrowserRouter>
+    </HashRouter>
+
 );
